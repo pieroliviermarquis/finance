44f5142b19b37dd89e608d5566798b5fd4e0efd5
pieroliviermarquis <pierolivier.marquis@gmail.com>
2021-02-12 15:27:28 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='financial',
+    version='1.0',
+    packages=['financial',],
+    license=Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
