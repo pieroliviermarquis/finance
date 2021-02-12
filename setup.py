1438df35be7c89a3a5076d303ce41888f04774f9
pieroliviermarquis <pierolivier.marquis@gmail.com>
2021-02-12 15:31:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,5 @@ from distutils.core import setup
 setup(
     name='finance',
     version='1.0',
-    packages=['finance',],
-    license=Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open('README.txt').read(),
+    packages=find_packages(include=['finance',])
 )
