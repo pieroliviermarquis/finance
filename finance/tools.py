908a24b6b4cfbd8f3f5986b2c7495d342ff6553f
pieroliviermarquis <pierolivier.marquis@gmail.com>
2021-02-12 16:24:19 -0500

    Update tools.py

--- finance/tools.py
+++ finance/tools.py
@@ -6,7 +6,6 @@ import requests
 def get_fred_data(series):
 
   df_all = pd.DataFrame()
-
   for name, serie in series.items():
     r = requests.get('https://api.stlouisfed.org/fred/series/observations?series_id=' + str(serie) + '&api_key=465e1b96e52576cf38130016ce9994d2&file_type=json').json()
     df = pd.DataFrame.from_dict(r['observations'])
@@ -15,7 +14,6 @@ def get_fred_data(series):
     df.rename(columns={'value': name}, inplace=True)
     df.index = pd.to_datetime(df.index)
     df_all = pd.concat([df_all, df], axis=1)
-
   return df_all
 
 
@@ -25,5 +23,4 @@ def clean_fred_data(data):
   cols = data.columns
   data[cols] = data[cols].apply(pd.to_numeric, errors='coerce', axis=1)
   data.dropna(inplace=True)
-
   return data
